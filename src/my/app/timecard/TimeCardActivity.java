9d1ba1abfa9d5c1384a7b78644a271f30147d045
Daisuke Jinno <jinno@sec.co.jp>
2013-03-02 23:16:01 +0900

    メールで送信機能を追加

--- src/my/app/timecard/TimeCardActivity.java
+++ src/my/app/timecard/TimeCardActivity.java
@@ -14,30 +14,40 @@ import java.util.Calendar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.DatePicker.OnDateChangedListener;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.TimePicker.OnTimeChangedListener;
+import android.widget.Toast;
 
-public class TimeCardActivity extends Activity implements
-		OnDateChangedListener, OnTimeChangedListener {
+public class TimeCardActivity extends Activity implements OnDateChangedListener,
+		OnTimeChangedListener {
 
 	static private final String TAG = "TimeCardActivity";
 
 	static private final String LOG_DIR = "/mnt/sdcard/Android/data/my.app.timecard/";
 	static private final String LOG_FILE = LOG_DIR + "log.dat";
 
+	private static final int MENU_ID_ADDRESS = (Menu.FIRST + 1);
+
 	private Button mUp = null;
 	private Button mDown = null;
 	private Button mMoveStart = null;
 	private Button mMoveFinish = null;
 	private Button mClear = null;
+	private Button mMail = null;
 
 	DatePicker mDatePicker = null;
 	TimePicker mTimePicker = null;
@@ -57,13 +67,13 @@ public class TimeCardActivity extends Activity implements
 		File dir = new File(LOG_DIR);
 		if (!dir.exists()) {
 			boolean ret = dir.mkdir();
-			 if (!ret) {
-				 Log.e(TAG, "directory not created. : " + LOG_DIR);	
-				 finish();
-				 return;
-			 }
+			if (!ret) {
+				Log.e(TAG, "directory not created. : " + LOG_DIR);
+				finish();
+				return;
+			}
 		}
-		
+
 		mDatePicker = (DatePicker) findViewById(R.id.DatePicker);
 		mTimePicker = (TimePicker) findViewById(R.id.TimePicker);
 		mLogView = (TextView) findViewById(R.id.Text);
@@ -72,6 +82,7 @@ public class TimeCardActivity extends Activity implements
 		mMoveStart = (Button) findViewById(R.id.move_start);
 		mMoveFinish = (Button) findViewById(R.id.move_finish);
 		mClear = (Button) findViewById(R.id.clear);
+		mMail = (Button) findViewById(R.id.mail);
 
 		Calendar c = Calendar.getInstance();
 		mYear = c.get(Calendar.YEAR);
@@ -86,8 +97,7 @@ public class TimeCardActivity extends Activity implements
 		String cancel = getString(R.string.cancel);
 		String confirm = getString(R.string.confirm);
 		String msg = getString(R.string.clear_msg);
-		final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-				this);
+		final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
 		alertDialogBuilder.setTitle(confirm);
 		alertDialogBuilder.setMessage(msg);
 		alertDialogBuilder.setPositiveButton(ok,
@@ -112,13 +122,18 @@ public class TimeCardActivity extends Activity implements
 				mClearDialog.show();
 			}
 		});
+		mMail.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				sendMail(loadAddress());
+			}
+		});
 
 		readLog();
 	}
-	
+
 	private class ProccessButtonClickListener implements OnClickListener {
 		public void onClick(View view) {
-			Button btn = (Button)view;
+			Button btn = (Button) view;
 			writeLog(btn.getText());
 		}
 	}
@@ -197,6 +212,79 @@ public class TimeCardActivity extends Activity implements
 		}
 		mLogView.setText(mLog);
 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// メニューアイテムを追加します
+		menu.add(Menu.NONE, MENU_ID_ADDRESS, Menu.NONE, "set address");
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	// オプションメニューが表示される度に呼び出されます
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	// オプションメニューアイテムが選択された時に呼び出されます
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean ret = true;
+		switch (item.getItemId()) {
+		default:
+			ret = super.onOptionsItemSelected(item);
+			break;
+		case MENU_ID_ADDRESS:
+			// テキスト入力を受け付けるビューを作成します。
+			final EditText editView = new EditText(TimeCardActivity.this);
+			editView.setText(loadAddress());
+			new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_info)
+					.setTitle("メールアドレスを入力してください")
+					// setViewにてビューを設定します。
+					.setView(editView)
+					.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int whichButton) {
+							// 入力した文字をトースト出力する
+							Toast.makeText(TimeCardActivity.this, editView.getText().toString(),
+									Toast.LENGTH_SHORT).show();
+							String address = editView.getText().toString();
+							saveAddress(address);
+						}
+					}).setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int whichButton) {
+						}
+					}).show();
+			
+			ret = true;
+			break;
+		}
+		return ret;
+	}
+
+	private void sendMail(String address) {
+		readLog();
+		String[] email = { address };
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_EMAIL, email);
+		intent.putExtra(Intent.EXTRA_SUBJECT, "TimeCard");
+		intent.putExtra(Intent.EXTRA_TEXT, mLog);
+		startActivity(intent);
+	}
+
+	private void saveAddress(String address) {
+		SharedPreferences sp = getSharedPreferences("address", MODE_PRIVATE);
+		SharedPreferences.Editor editor = sp.edit();
+		editor.putString("address", address);
+		editor.commit();
+	}
+
+	private String loadAddress() {
+		SharedPreferences sp = getSharedPreferences("address", MODE_PRIVATE);
+		String address = sp.getString("address", "");
+		return address;
 	}
 
 }
