54f0cd276cdeab618cb6d53c071a560832fea7d3
Kousei Iwabuchi <sec.d04@gmail.com>
2013-11-17 09:13:07 +0900

    Android 4.0以上向け画面レイアウト変更

--- src/my/app/timecard/TimeCardActivity.java
+++ src/my/app/timecard/TimeCardActivity.java
@@ -4,30 +4,35 @@ import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.TimePicker.OnTimeChangedListener;
 import android.widget.Toast;
 
-public class TimeCardActivity extends Activity implements OnDateChangedListener,
-		OnTimeChangedListener {
+public class TimeCardActivity extends Activity {
 
 	static private final String TAG = "TimeCardActivity";
 
 	private static final int MENU_ID_ADDRESS = (Menu.FIRST + 1);
 
+	private Button mDateChange = null;
+	private Button mTimeChange = null;
 	private Button mUp = null;
 	private Button mDown = null;
 	private Button mMoveStart = null;
@@ -35,14 +40,18 @@ public class TimeCardActivity extends Activity implements OnDateChangedListener,
 	private Button mClear = null;
 	private Button mMail = null;
 
-	DatePicker mDatePicker = null;
-	TimePicker mTimePicker = null;
+	DatePickerDialog mDatePickerDialog = null;
+	TimePickerDialog mTimePickerDialog = null;
+
+	TextView mTextDate = null;
+	TextView mTextTime = null;
 	TextView mLogView = null;
+	
 
 	AlertDialog mClearDialog = null;
 
 	String mLog = new String();
-	int mYear, mMonth, mDay, mHour, mMin;
+	private Calendar mCurrentTime;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -55,9 +64,11 @@ public class TimeCardActivity extends Activity implements OnDateChangedListener,
 			finish();
 		}
 
-		mDatePicker = (DatePicker) findViewById(R.id.DatePicker);
-		mTimePicker = (TimePicker) findViewById(R.id.TimePicker);
-		mLogView = (TextView) findViewById(R.id.Text);
+		mLogView = (TextView) findViewById(R.id.text_log);
+		mTextDate = (TextView) findViewById(R.id.text_date);
+		mTextTime = (TextView) findViewById(R.id.text_time);
+		mDateChange = (Button) findViewById(R.id.change_date);
+		mTimeChange = (Button) findViewById(R.id.change_time);
 		mUp = (Button) findViewById(R.id.up);
 		mDown = (Button) findViewById(R.id.down);
 		mMoveStart = (Button) findViewById(R.id.move_start);
@@ -65,17 +76,90 @@ public class TimeCardActivity extends Activity implements OnDateChangedListener,
 		mClear = (Button) findViewById(R.id.clear);
 		mMail = (Button) findViewById(R.id.mail);
 
-		Calendar c = Calendar.getInstance();
-		mYear = c.get(Calendar.YEAR);
-		mMonth = c.get(Calendar.MONTH);
-		mDay = c.get(Calendar.DAY_OF_MONTH);
-		mHour = c.get(Calendar.HOUR_OF_DAY);
-		mMin = c.get(Calendar.MINUTE);
-		mDatePicker.init(mYear, mMonth, mDay, this);
-		mTimePicker.setOnTimeChangedListener(this);
+		mDateChange.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDatePickerDialog.show();
+			}
+		});
+		mTimeChange.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mTimePickerDialog.show();
+			}
+		});
+		mUp.setOnClickListener(new ProccessButtonClickListener());
+		mDown.setOnClickListener(new ProccessButtonClickListener());
+		mMoveStart.setOnClickListener(new ProccessButtonClickListener());
+		mMoveFinish.setOnClickListener(new ProccessButtonClickListener());
+		mClear.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				mClearDialog.show();
+			}
+		});
+		mMail.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				sendMail(loadAddress());
+			}
+		});
 
+		mCurrentTime = Calendar.getInstance();
+		setCurrentTime();
+		createDialog();
+
+		mLog = LogManager.load();
+		mLogView.setText(mLog);
+	}
+
+	private void setCurrentTime() {
+		CharSequence time = DateFormat.format("yyyy/MM/dd", mCurrentTime);
+		mTextDate.setText(time);
+		Log.d(TAG, "setCurrentTime : Date = " + time);
+		time = DateFormat.format("kk:mm:ss", mCurrentTime);
+		mTextTime.setText(time);
+		Log.d(TAG, "setCurrentTime : time = " + time);
+	}
+	
+	private void createDialog() {
+ 		OnDateSetListener dateListener = new OnDateSetListener(){
+			
+			@Override
+			public void onDateSet(DatePicker view, int year, int monthOfYear,
+					int dayOfMonth) {
+				Log.d(TAG,"onDateSet : year = " + year + ", month = " + monthOfYear + ", day = " + dayOfMonth);
+				mCurrentTime.set(year, monthOfYear, dayOfMonth);
+				mCurrentTime.set(Calendar.YEAR, year);
+				mCurrentTime.set(Calendar.MONTH, monthOfYear);
+				mCurrentTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+				setCurrentTime();
+		}};
+
+		OnTimeSetListener timeListener = new OnTimeSetListener() {
+
+			@Override
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				Log.d(TAG,"onDateSet : hour = " + hourOfDay + ", minute = " + minute);
+				mCurrentTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+				mCurrentTime.set(Calendar.MINUTE, minute);
+				setCurrentTime();
+			}
+		};
+		
+		mCurrentTime = Calendar.getInstance();
+		mDatePickerDialog = new DatePickerDialog(this, dateListener,
+				mCurrentTime.get(Calendar.YEAR),
+				mCurrentTime.get(Calendar.MONTH),
+				mCurrentTime.get(Calendar.DAY_OF_MONTH));
+		mTimePickerDialog = new TimePickerDialog(this, timeListener,
+				mCurrentTime.get(Calendar.HOUR_OF_DAY),
+				mCurrentTime.get(Calendar.MINUTE), true);
+		mDatePickerDialog.setTitle(R.string.change_date);
+		mTimePickerDialog.setTitle(R.string.change_time);
+		
 		String ok = getString(R.string.ok);
 		String cancel = getString(R.string.cancel);
+
+		// クリア確認ダイアログ
 		String confirm = getString(R.string.confirm);
 		String msg = getString(R.string.clear_msg);
 		final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
@@ -92,46 +176,20 @@ public class TimeCardActivity extends Activity implements OnDateChangedListener,
 		alertDialogBuilder.setNegativeButton(cancel, null);
 		mClearDialog = alertDialogBuilder.create();
 
-		mUp.setOnClickListener(new ProccessButtonClickListener());
-		mDown.setOnClickListener(new ProccessButtonClickListener());
-		mMoveStart.setOnClickListener(new ProccessButtonClickListener());
-		mMoveFinish.setOnClickListener(new ProccessButtonClickListener());
-		mClear.setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {
-				mClearDialog.show();
-			}
-		});
-		mMail.setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {
-				sendMail(loadAddress());
-			}
-		});
-
-		mLog = LogManager.load();
-		mLogView.setText(mLog);
 	}
 
 	private class ProccessButtonClickListener implements OnClickListener {
 		public void onClick(View view) {
 			Button btn = (Button)view;
 			
-			String str = String.format("%04d/%02d/%02d %02d:%02d %s\n", mYear, mMonth + 1,
-					mDay, mHour, mMin, btn.getText());
+			String str = String.format("%s %s %s\n",
+					mTextDate.getText(),
+					mTextTime.getText(),
+					btn.getText());
 			mLog = str + mLog;
 			mLogView.setText(mLog);
 			LogManager.write(mLog);
 		}
-	}
-
-	public void onDateChanged(DatePicker view, int year, int month, int day) {
-		mYear = year;
-		mMonth = month;
-		mDay = day;
-	}
-
-	public void onTimeChanged(TimePicker arg0, int hour, int min) {
-		mHour = hour;
-		mMin = min;
 	}
 
 	@Override
